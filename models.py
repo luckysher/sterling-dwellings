e005fb5aa25b9621b122f964c521ed51e60312aa
shersingh <luckyshera95@gmail.com>
2026-04-23 13:08:16 +0530

    Added property type extended

--- models.py
+++ models.py
@@ -1,6 +1,20 @@
+from typing import Annotated, Optional, Literal
+from pydantic.json_schema import SkipJsonSchema
+from sqlmodel import Field, SQLModel
 from enum import Enum
+import datetime
 
-class PropertyType(str, Enum):
+class PropertyTypeBase(str, Enum):
     RESIDENTIAL = "residential"
     COMMERCIAL = "commercial"
+
+class PropertyTypeExtended(str, Enum):
+    FLAT = "flat"
+    RESIDENTIAL_LAND = "residential_land"
+    SERVICE_APARTMENT = "service_apartment"
+    STUDIO_APARTMENT = "studio_apartment"
+    FARM_HOUSE = "farm_house"
+    BUILDER_FLOOR = "builder_floor"
+    VILLA = "villa"
+    OTHER = "other"
 
