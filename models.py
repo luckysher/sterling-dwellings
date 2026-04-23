af2185afe7801c22beb8cc78ba9aa951f5f9a7f2
shersingh <luckyshera95@gmail.com>
2026-04-23 13:09:05 +0530

    added user dealer model

--- models.py
+++ models.py
@@ -18,3 +18,20 @@ class PropertyTypeExtended(str, Enum):
     VILLA = "villa"
     OTHER = "other"
 
+class User(SQLBaseModel):
+    id: Annotated[int, SkipJsonSchema()] = Field(primary_key=True)
+    name: str = Field(max_length=64, nullable=False)
+    email: str = Field(unique=True, max_length=64, nullable=False)
+    password: Optional[str]
+    contact_no: str = Field(nullable=False, default="")
+    active: Annotated[bool, SkipJsonSchema()] = Field(default=False, nullable=False)
+    is_admin: Annotated[bool, SkipJsonSchema()] = Field(default=False, nullable=False)
+    last_login: Annotated[datetime.datetime | None, SkipJsonSchema()] = Field(default=None)
+    created_at: Annotated[datetime.datetime, SkipJsonSchema()] = Field(default=datetime.datetime.now())
+
+class Dealer(SQLBaseModel):
+    user_id: int = Field(default=None, foreign_key="user.id")
+    dealer_pic: str = Field(max_length=256, nullable=False, default="")
+    name: str = Field(max_length=64, nullable=False)
+    address: str = Field(max_length=128, nullable=False, default="")
+
