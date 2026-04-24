1fe3fc4ca4f1f0bee2261a7c62ead54e41ea79c2
shersingh <luckyshera95@gmail.com>
2026-04-24 11:44:18 +0530

    added base model

--- models.py
+++ models.py
@@ -4,54 +4,7 @@ from sqlmodel import Field, SQLModel
 from enum import Enum
 import datetime
 
-class PropertyTypeBase(str, Enum):
-    RESIDENTIAL = "residential"
-    COMMERCIAL = "commercial"
 
-class PropertyTypeExtended(str, Enum):
-    FLAT = "flat"
-    RESIDENTIAL_LAND = "residential_land"
-    SERVICE_APARTMENT = "service_apartment"
-    STUDIO_APARTMENT = "studio_apartment"
-    FARM_HOUSE = "farm_house"
-    BUILDER_FLOOR = "builder_floor"
-    VILLA = "villa"
-    OTHER = "other"
-
-class User(SQLBaseModel):
+class BaseSQLModel(SQLModel, table=True):
     id: Annotated[int, SkipJsonSchema()] = Field(primary_key=True)
-    name: str = Field(max_length=64, nullable=False)
-    email: str = Field(unique=True, max_length=64, nullable=False)
-    password: Optional[str]
-    contact_no: str = Field(nullable=False, default="")
-    active: Annotated[bool, SkipJsonSchema()] = Field(default=False, nullable=False)
-    is_admin: Annotated[bool, SkipJsonSchema()] = Field(default=False, nullable=False)
-    last_login: Annotated[datetime.datetime | None, SkipJsonSchema()] = Field(default=None)
     created_at: Annotated[datetime.datetime, SkipJsonSchema()] = Field(default=datetime.datetime.now())
-
-class Dealer(SQLBaseModel):
-    user_id: int = Field(default=None, foreign_key="user.id")
-    dealer_pic: str = Field(max_length=256, nullable=False, default="")
-    name: str = Field(max_length=64, nullable=False)
-    address: str = Field(max_length=128, nullable=False, default="")
-
-
-class Property(SQLModel, table=True):
-    id: Annotated[int, SkipJsonSchema()] = Field(primary_key=True)
-    # basic info
-    name: str = Field(max_length=64, nullable=False)
-    description: str = Field(max_length=128, nullable=False)
-    # type
-    property_type_1: Annotated[PropertyTypeBase, SkipJsonSchema()] = Field(default=PropertyTypeBase.RESIDENTIAL)
-    property_type_2: Annotated[PropertyTypeExtended, SkipJsonSchema()] = Field(default=PropertyTypeExtended.RESIDENTIAL_LAND)
-
-    # location
-    area: str = Field(max_length=64, nullable=False)
-    city: str = Field(unique=True, max_length=64, nullable=False)
-
-    # features
-    price_in_inr: int = Field(nullable=False)
-    area_in_sqrft: int = Field(nullable=False)
-    no_of_room: int = Field(nullable=False)
-    no_of_bath: int = Field(nullable=False)
-
