40f24811a405967f7b0a5dd356b5fa756b3a73d6
shersingh <luckyshera95@gmail.com>
2026-04-25 20:53:43 +0530

    Added model for villa

--- models.py
+++ models.py
@@ -2,9 +2,19 @@ from typing import Annotated, Optional, Literal
 from pydantic.json_schema import SkipJsonSchema
 from sqlmodel import Field, SQLModel
 from enum import Enum
+from decimal import Decimal
 import datetime
+from datetime import date
 
 
 class BaseSQLModel(SQLModel, table=True):
     id: Annotated[int, SkipJsonSchema()] = Field(primary_key=True)
     created_at: Annotated[datetime.datetime, SkipJsonSchema()] = Field(default=datetime.datetime.now())
+
+class Villa(BaseSQLModel):
+    property_id: int = Field(nullable=False, foreign_key="property.id")
+    price_per_night: float
+    no_of_bedrooms: int
+    no_of_bathrooms: int
+    has_pool: bool = False
+    description: Optional[str] = None
